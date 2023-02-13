297a00e8f7a46aae84b5abcb3f37df929c30529f
AgnoSantiago <developer.aspt@gmail.com>
2023-02-13 20:41:20 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 public class ClienteRequest {
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	@Enumerated(EnumType.STRING)
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
 
 }
