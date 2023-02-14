50dfa5b10d7de3a3b95fcce42156321fbc011bf1
AgnoSantiago <developer.aspt@gmail.com>
2023-02-14 14:37:10 -0300

    implementa cliente Infra Repository

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -4,17 +4,22 @@ import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
+
 public class ClienteInfraRepository implements ClienteRepository {
+
+	private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
 
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[start] - ClienteInfraRepository - salvaCliente");
+		clienteSpringDataJPARepository.save(cliente);
 		log.info("[finish] - ClienteInfraRepository - salvaCliente");
 		return cliente;
 	}
-
 }
