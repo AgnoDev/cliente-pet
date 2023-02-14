bd28d5ff02912f7350d1357006898d23f84d63d9
AgnoSantiago <developer.aspt@gmail.com>
2023-02-14 15:40:45 -0300

    implementa metodo convert do ClienteListResponse

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -2,9 +2,12 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
@@ -13,7 +16,14 @@ public class ClienteListResponse {
 	private String cpf;
 
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream().map(ClienteListResponse::new).collect(Collectors.toList());
 	}
 
+	public ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+		this.cpf = cliente.getCpf();
+	}
 }
