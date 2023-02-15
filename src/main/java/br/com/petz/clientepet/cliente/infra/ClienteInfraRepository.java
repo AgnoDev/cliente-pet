9ac84ed38974280a5c884795fa9aefab6d55ab99
AgnoSantiago <developer.aspt@gmail.com>
2023-02-15 14:01:41 -0300

    tratamento de exceção de duplicidade

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -3,10 +3,13 @@ package br.com.petz.clientepet.cliente.infra;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import br.com.petz.clientepet.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -21,7 +24,11 @@ public class ClienteInfraRepository implements ClienteRepository {
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[start] - ClienteInfraRepository - salvaCliente");
-		clienteSpringDataJPARepository.save(cliente);
+		try {
+			clienteSpringDataJPARepository.save(cliente);
+		} catch (DataIntegrityViolationException e) {
+			throw APIException.build(HttpStatus.BAD_REQUEST, "E-mail/CPF já cadastrado(s).", e);
+		}
 		log.info("[finish] - ClienteInfraRepository - salvaCliente");
 		return cliente;
 	}
